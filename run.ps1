5a83af1f737c4f0a09260dc8030e73e485b9bd79
Chigozirim Chukwu <nobleman.code@gmx.com>
2020-03-11 18:11:47 -0600

    use script for running networks and use pipenv

---
+++ run.ps1
@@ -0,0 +1,21 @@
+param (
+    [string]$script,
+    [switch]$help = $false
+)
+
+if ($help) {
+    Write-Output "Options:"
+    Write-Output "-script: path/to/script"
+    Write-Output "-help: display this help"
+    Exit
+}
+
+if (-not $script) {
+    Write-Output "No script provided. Use -help for more info"
+    Exit 1
+}
+
+# Add the root of the project to the python path
+$Env:PYTHONPATH += "$([IO.Path]::PathSeparator)${PSScriptRoot}"
+
+pipenv run python $script
